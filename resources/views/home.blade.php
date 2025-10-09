248cbfc1879d5d71de148e77101ba8044810ba80
Francisco <fcomontsep@gmail.com>
2025-10-09 17:03:58 -0300

    Agrego: Experimentación Plantillas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,38 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 12</title>
-
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-</head>
-
-<body>
+@section('content')
     <div class="max-w-4xl mx-auto px-4">
-        <h1>Bienvenido a la página principal en creación</h1>
-		A continuación van Alerts hechos como componentes en components.
+        <h1>Bienvenido a la página principal en creación con componentes</h1>
+        A continuación van Alerts hechos como componentes en components.
 
-		<x-alert type="info">
-			Este es el valor del slot que viene desde blade.php
-		</x-alert>
-		<x-alert type="danger">
-			Este es el valor del slot que viene desde blade.php
-		</x-alert>
+        <x-alert type="info">
+            Este es el valor del slot que viene desde blade.php
+        </x-alert>
+        <x-alert type="danger">
+            Este es el valor del slot que viene desde blade.php
+        </x-alert>
 
-		A continuación van Alerts hechos como componentes en App.
-		
-		<x-alert2 type="info">
-			Este es el valor del slot que viene desde blade.php
-		</x-alert2>
-		<x-alert2 type="danger">
-			Este es el valor del slot que viene desde blade.php
-		</x-alert2>
+        A continuación van Alertss hechos como componentes en App.
+
+        <x-alert2 type="info">
+            Este es el valor del slot que viene desde blade.php
+        </x-alert2>
+        <x-alert2 type="danger">
+            Este es el valor del slot que viene desde blade.php
+        </x-alert2>
 
     </div>
-
-</body>
-
-</html>
+@endsection
